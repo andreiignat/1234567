dbb54982d5fa318e835ee8bc2d495d54132c73da
andrei ignat <>
2023-05-06 16:38:41 +0300

    Добавляем Block схему

--- FINALTASK/task/Program.cs
+++ FINALTASK/task/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.*/
+
+
+Console.Write("Введите колличество элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+string[] array = CreateArray(size);
+string[] NewArray = new string[array.Length];  // Создаем новый массив
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        NewArray[i] = array[i];
+    }
+}
+
+Console.WriteLine($"Новый массив состоит из :");
+PrintArray(NewArray);
+
+string[] CreateArray(int size) // Метод для создания строчного массива с клавиатуры
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i}-й элемент массива : ");
+        array[i] = Console.ReadLine()!; //Заполняем его
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void PrintArray(string[] array)  // Метод для печати массива
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
